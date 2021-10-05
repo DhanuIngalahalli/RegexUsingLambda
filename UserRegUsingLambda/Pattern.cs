86b0dc6b7b2b385769c4b2016a45deef56c2fdb5
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-10-05 11:43:09 -0700

    User reg using lambda expression

---
+++ UserRegUsingLambda/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegUsingLambda
+{
+    public class Patterns
+    {
+        public Func<string, string, bool> IsValidPasswordRule = (x, y) => Regex.IsMatch(x, y);
+        public Func<string, string, bool> IsValidFirstName = (x, y) => Regex.IsMatch(x, y);
+        public Func<string, string, bool> IsValidLastName = (x, y) => Regex.IsMatch(x, y);
+        public Func<string, string, bool> IsValidEmail = (x, y) => Regex.IsMatch(x, y);
+        public Func<string, string, bool> IsValidPhoneNo = (x, y) => Regex.IsMatch(x, y);
+    }
+}
