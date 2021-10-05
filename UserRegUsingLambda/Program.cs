e9163f7a3692ec8394a1398673cf9436f8f075b2
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-10-05 11:33:16 -0700

    Hello

---
+++ UserRegUsingLambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegUsingLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
